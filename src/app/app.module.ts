785aa7e52aabb74c200de1e27d42abb1a6713551
leiv03 <nadia.guarisco@anima.edu.uy>
2022-05-03 17:38:15 -0300

    basics apiController tested

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ClimaComponent } from './clima/clima.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClimaComponent
   ],
   imports: [
     BrowserModule,
--- src/app/services/clima.service.ts
+++ src/app/services/clima.service.ts
@@ -9,28 +9,27 @@ import { Observable } from 'rxjs';
 })
 export class ClimaService {
 
-  constructor( private http: HttpClient ) { }
+  //variables de entorno
+  private apiUrl = environment.weatherApiBaseUrl;
+  private keyUrl = environment.apikey;
 
-  cargarClima(){
-    const api_key = '06bbe6a2d2134dad84712501222104';
-    const localidad = 'Rivera';
-    const dias = 1;
-    const calidad_del_aire = 'no';
-    const alertas = 'no';
-    const url = 'http://api.weatherapi.com/v1/forecast.json';
-    const options = { params: new HttpParams({fromString: 'key='+api_key+'&q='+localidad+'&days='+dias+'&aqi='+calidad_del_aire+'&alerts='+alertas})};
-    return this.http.get(url, options);
+  //inicializar httpClient
+  constructor(private http: HttpClient) { }
 
-  }
-
-  getWeatherData(cityName:string, days:string, air_quality:string, alerts:string): Observable<WeatherData>{
-    return this.http.get<WeatherData>(environment.weatherApiBaseUrl, {
+  //metodo para traer clima
+  getWeatherData(
+    cityName: string,
+    days: string,
+    air_quality: string,
+    alerts: string
+  ): Observable<WeatherData> {
+    return this.http.get<WeatherData>(this.apiUrl, {
       params: new HttpParams()
-      .set('key', environment.apikey)
-      .set('q', cityName)
-      .set('days', days)
-      .set('aqi', air_quality)
-      .set('alerts', alerts)
+        .set('key', this.keyUrl)
+        .set('q', cityName)
+        .set('days', days)
+        .set('aqi', air_quality)
+        .set('alerts', alerts)
     })
   }
 
